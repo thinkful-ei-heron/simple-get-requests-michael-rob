700f85fdfd7ed9b53fc09434754b98b70c5895ad
Michael Kirsch <michaelskirsch@gmail.com>
2019-09-27 10:22:36 -0600

    version 1 (logs to console) working

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 'use strict';
 
-function getDogImage() {
-  fetch('https://dog.ceo/api/breeds/image/random')
+function getDogImages(num) {
+  fetch(`https://dog.ceo/api/breeds/image/random/${num}`)
     .then(response => response.json())
-    .then(responseJson => console.log(responseJson));
+    .then(responseJson => console.log(responseJson))
+    .catch(error => console.log(error));
 }
 
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
-    getDogImage();
+    const num = $('#num').val() //doesn't matter which one we watch
+    getDogImages(num);
+  });
+}
+function watchSlider() {
+  $('#slider').on('input', event => {
+    $('#num').val(event.target.value);
+  });
+}
+function watchNumberBox() {
+  $('#num').on('input', event => {
+    $('#slider').val(event.target.value);
   });
 }
 
 $(function() {
   console.log('App loaded! Waiting for submit!');
   watchForm();
+  watchNumberBox();
+  watchSlider();
 });
